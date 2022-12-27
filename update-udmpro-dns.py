408cc23cb17143824685e895af06c75cadf7462b
cdchris12 <chris.davis@amazee.io>
2022-12-27 15:40:26 -0600

    Updated README.md and links to old script name, added ending comments

--- update-udmpro-dns.py
+++ update-udmpro-dns.py
@@ -13,9 +13,10 @@ import re
 import sys
 import netrc
 import tempfile
+import urllib3
 from argparse import ArgumentParser
 
-import urllib3
+# External modules
 import requests
 import paramiko
 from scp import SCPClient
@@ -29,6 +30,7 @@ def get_configured_clients(session: requests.Session, baseurl: str, site: str):
   r = session.get(f'{baseurl}/proxy/network/api/s/{site}/list/user', verify=False)
   r.raise_for_status()
   return r.json()['data']
+# End def
 
 def get_active_clients(session: requests.Session, baseurl: str, site: str):
   """Get active clients from UDMPro"""
@@ -36,6 +38,7 @@ def get_active_clients(session: requests.Session, baseurl: str, site: str):
   r = session.get(f'{baseurl}/proxy/network/api/s/{site}/stat/sta', verify=False)
   r.raise_for_status()
   return r.json()['data']
+# End def
 
 def get_configured_networks(session: requests.Session, baseurl: str, site: str):
   """Get configured networks from UDMPro"""
@@ -43,15 +46,18 @@ def get_configured_networks(session: requests.Session, baseurl: str, site: str):
   r = session.get(f'{baseurl}/proxy/network/api/s/{site}/rest/networkconf', verify=False)
   r.raise_for_status()
   return r.json()['data']
+# End def
 
 def build_fqdn(client: dict, networks: dict):
   """Return fully qualified domain name for client on network"""
 
   if client['network_id'] in networks:
     return f"{client['name']}.{networks[client['network_id']]}"
+  # End if
 
   # default to .home.arpa, per IETF RFC8375
   return f"{client['name']}.home.arpa"
+# End def
 
 def get_clients(baseurl: str, username: str, password: str, site: str, fixed_only: bool, verbose: int):
   """Get clients and networks from UDMPro"""
@@ -70,6 +76,8 @@ def get_clients(baseurl: str, username: str, password: str, site: str, fixed_onl
   for n in get_configured_networks(s, baseurl, site):
     if 'domain_name' in n:
       networks[n['_id']] = n['domain_name']
+    # End if
+  # End for
 
   # Add clients with alias and reserved IP
   clients = {}
@@ -79,6 +87,8 @@ def get_clients(baseurl: str, username: str, password: str, site: str, fixed_onl
       c['name'] = c['name'].lower()
       fqdn = build_fqdn(c, networks)
       clients[c['mac']] = {'name': c['name'], 'fqdn': fqdn, 'ip': c['fixed_ip']}
+    # End if
+  # End for
 
   if not fixed_only:
     # Add active clients with alias
@@ -88,11 +98,15 @@ def get_clients(baseurl: str, username: str, password: str, site: str, fixed_onl
         c['name'] = c['name'].lower()
         fqdn = build_fqdn(c, networks)
         clients[c['mac']] = {'name': c['name'], 'fqdn': fqdn, 'ip': c['ip']}
+      # End if
+    # End for
+  # End if
   
   # Return a list of clients filtered on dns-friendly names and sorted by IP
   friendly_clients = [c for c in clients.values() if re.search('^[a-zA-Z0-9-]+$', c['name'])] 
   if verbose>2: print('Client list obtained successfully!')
   return sorted(friendly_clients, key=lambda i: i['name'])
+# End def
 
 def merge_dnsmasq_conf(hosts: list, custom_path:str, localpath: str, verbose: int):
   """Merge discovered clients with custom options file into temp file to upload"""
@@ -101,11 +115,16 @@ def merge_dnsmasq_conf(hosts: list, custom_path:str, localpath: str, verbose: in
     # discovered clients
     for ip, name, fqdn in hosts:
       outfile.write(f'host-record={fqdn},{name},{ip}\n')
+    # End for
 
     # local custom options
     with open(custom_path, 'r') as localfile:
       for line in localfile:
         outfile.write(line)
+      # End for
+    # End with
+  # End with
+# End def
 
 def scp_dnsmasq(localpath: str, ssh_username: str, ssh_password: str, ssh_address: str, verbose: int):
   """Copy merged temp file to UDMPro and restart DNSMasq"""
@@ -123,11 +142,13 @@ def scp_dnsmasq(localpath: str, ssh_username: str, ssh_password: str, ssh_addres
   if verbose>2: print('Pushing new dns-alias.conf file...')
   with SCPClient(ssh_client.get_transport()) as scp:
     scp.put(localpath, filepath)
+  # End with
 
   # Restart dnsmasq on the UDM Pro
   if verbose>2: print('Restarting dnsmasq service...')
   ssh_client.exec_command("""killall dnsmasq""")
   #ssh_client.exec_command("""kill `cat /run/dnsmasq.pid`""")
+# End def
 
 
 def main():
@@ -154,22 +175,26 @@ def main():
     if args.netrc not in passes.hosts:
       print(f'Unifi API host "{args.netrc}" not in netrc file!')
       exit(1)
+    # End if
 
     (login, account, password) = passes.authenticators(args.netrc)
     args.baseurl = args.netrc
     args.username = login
     args.password = password
+  # End if
 
   if args.ssh:
     if args.ssh not in passes.hosts:
       print(f'SSH host "{args.ssh}" not in netrc file!')
       exit(1)
+    # End if
 
     passes = netrc.netrc()
     (login, _, password) = passes.authenticators(args.ssh)
     args.ssh_address = args.ssh
     args.ssh_username = login
     args.ssh_password = password
+  # End if
 
   # Get list of hosts and IPs
   try:
@@ -177,11 +202,14 @@ def main():
 
     hosts = []
     for c in get_clients(args.baseurl, args.username, args.password, args.site, args.fixed_only, args.verbose):
+      if args.verbose>2: print(f"Found host - IP: {c['ip']}\tName: {c['name']}\tFQDN:{c['fqdn']}")
       hosts.append((c['ip'], c['name'], c['fqdn']))
+    # End for
 
   except requests.exceptions.ConnectionError:
     print(f'Could not connect to unifi controller at {args.baseurl}', file=sys.stderr)
     exit(1)
+  # End try/except block
 
   if args.verbose:
     if hosts:
@@ -189,8 +217,11 @@ def main():
       for host in hosts:
         domainname = host[2][len(host[1])+1:]
         print(f'\t{host[0]:15} {host[1]} ({domainname})')
+      # End for
     else:
       print('No hosts found!')
+    # End if/else block
+  # End if
 
   # SCP list onto target UDM Pro
   (fp, tempfn) = tempfile.mkstemp()
@@ -200,6 +231,8 @@ def main():
   # Clean up local file
   if args.verbose>2: print('Cleaning up local files...')
   os.remove(tempfn)
+# End def
 
 if __name__ == '__main__':
   main()
+# End if
